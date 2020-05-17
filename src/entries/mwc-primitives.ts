0dd3bcb86bc7fc9507f426698fdabbc89879c720
Stephen Niedzielski <stephen@niedzielski.com>
2020-05-17 00:24:02 -0600

    [components] copy Spinner

--- src/entries/mwc-primitives.ts
+++ src/entries/mwc-primitives.ts
@@ -3,11 +3,13 @@ import MwCard from '@/components/card/Card.vue';
 import MwDivider from '@/components/divider/Divider.vue';
 import MwIcon from '@/components/icon/Icon.vue';
 import MwInput from '@/components/input/Input.vue';
+import MwSpinner from '@/components/spinner/Spinner.vue';
 
 export default {
 	MwButton,
 	MwCard,
 	MwDivider,
 	MwIcon,
-	MwInput
+	MwInput,
+	MwSpinner
 };
