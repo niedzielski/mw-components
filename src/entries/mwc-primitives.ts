6c490e1fb84b3660f3e8d27207f591ecb616bee1
Stephen Niedzielski <stephen@niedzielski.com>
2020-05-17 00:21:31 -0600

    [components] copy Input

--- src/entries/mwc-primitives.ts
+++ src/entries/mwc-primitives.ts
@@ -2,10 +2,12 @@ import MwButton from '@/components/button/Button.vue';
 import MwCard from '@/components/card/Card.vue';
 import MwDivider from '@/components/divider/Divider.vue';
 import MwIcon from '@/components/icon/Icon.vue';
+import MwInput from '@/components/input/Input.vue';
 
 export default {
 	MwButton,
 	MwCard,
 	MwDivider,
-	MwIcon
+	MwIcon,
+	MwInput
 };
