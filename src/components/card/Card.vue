05cb65249f382e2c670ca7c4d28af652dcb80f3c
Stephen Niedzielski <stephen@niedzielski.com>
2020-05-17 00:20:45 -0600

    [components] copy Card

---
+++ src/components/card/Card.vue
@@ -0,0 +1,41 @@
+<template>
+	<div :id="id" class="mw-ui-card">
+		<div class="mw-ui-card__title title" v-text="title" />
+		<div class="mw-ui-card__content">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend( {
+	name: 'MwCard',
+	props: {
+		id: {
+			type: String,
+			default: null
+		},
+		title: { type: String, default: '' }
+	}
+} );
+</script>
+
+<style lang="less">
+@import "../styles/mediawiki/mediawiki.less/mediawiki.ui/variables.less";
+@import "../styles/mediawiki/mediawiki.less/mediawiki.mixins.less";
+
+.mw-ui-card {
+	background-color: @background-color-base;
+	box-sizing: border-box;
+	margin-bottom: 1em;
+	border-radius: @border-radius-base;
+	padding: 8px;
+	.box-shadow(0 1px 1px rgba(0, 0, 0, 0.15));
+	.mw-ui-card__title {
+		height: 2em;
+		color: @colorGray5;
+	}
+}
+</style>
--- src/entries/mwc-primitives.ts
+++ src/entries/mwc-primitives.ts
@@ -1,9 +1,11 @@
 import MwButton from '@/components/button/Button.vue';
+import MwCard from '@/components/card/Card.vue';
 import MwDivider from '@/components/divider/Divider.vue';
 import MwIcon from '@/components/icon/Icon.vue';
 
 export default {
 	MwButton,
+	MwCard,
 	MwDivider,
 	MwIcon
 };
